f8d9d8e577aac1e2f7d41f95f4e566daf6312752
thebinsohail <anassohail.as@gmail.com>
2022-04-07 03:36:20 +0500

    done with subscribe and return status

---
+++ src/main/java/com/server/api/notify/NotifyController.java
@@ -0,0 +1,24 @@
+package com.server.api.notify;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
+@RestController
+@RequestMapping(path = "/subscribe")
+public class NotifyController {
+
+
+    private static final int [] STATUS_CODE={100,200,304,401,404,500};
+
+    @GetMapping
+    public int subscribe(){
+
+        int random=new Random().nextInt(STATUS_CODE.length);
+        return STATUS_CODE[random];
+
+    }
+
+}
